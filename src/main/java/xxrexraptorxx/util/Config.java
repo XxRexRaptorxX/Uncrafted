5146dab764403c201939315b1ba4b6230d5f80a6
XxRexRaptorxX <Raptor-Simon@web.de>
2022-08-18 19:21:23 +0200

    config re-enabled

--- src/main/java/xxrexraptorxx/util/Config.java
+++ src/main/java/xxrexraptorxx/util/Config.java
@@ -1,59 +1,50 @@
 package xxrexraptorxx.util;
 
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod.EventBusSubscriber
 public class Config {
-/**
+
     public static final String CATEGORY_GENERAL = "general";
 
-    public static ForgeConfigSpec SERVER_CONFIG;
     public static ForgeConfigSpec CLIENT_CONFIG;
+    public static ForgeConfigSpec SERVER_CONFIG;
 
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_SPAWNEGG_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_SKULL_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_OPERATOR_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_UNBREAKABLE_BLOCKS_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_SPAWNER_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_TOTEM_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_DRAGON_EGG_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_DISC_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_NETHER_STAR_RECIPES;
-    public static ForgeConfigSpec.BooleanValue ACTIVATE_UNCRAFTING_RECIPES;
+    public static ForgeConfigSpec.BooleanValue UPDATE_CHECKER;
+    public static ForgeConfigSpec.BooleanValue PATREON_REWARDS;
 
+    public static void init() {
+        initClient();
+        initServer();
 
-    static {
-        ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
-        ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_CONFIG);
 
-        CLIENT_BUILDER.comment("General settings").push(CATEGORY_GENERAL);
-
-        ACTIVATE_SPAWNEGG_RECIPES = CLIENT_BUILDER.comment("Activate spawnegg crafting recipes").define("spawneggRecipes", true);
-        ACTIVATE_SKULL_RECIPES = CLIENT_BUILDER.comment("Activate skull crafting recipes").define("skullRecipes", true);
-        ACTIVATE_OPERATOR_RECIPES = CLIENT_BUILDER.comment("Activate Operator crafting recipes like command blocks & structure blocks (these blocks are useless without rights and gamemode 1)").define("opderatorRecipes", false);
-        ACTIVATE_UNBREAKABLE_BLOCKS_RECIPES = CLIENT_BUILDER.comment("Activate crafting recipes for unbreakable blocks (like bedrock)").define("unbreakableBlocksRecipes", true);
-        ACTIVATE_SPAWNER_RECIPES = CLIENT_BUILDER.comment("Activate the crafting recipe for the spawner").define("spawnerRecipe", true);
-        ACTIVATE_DRAGON_EGG_RECIPES = CLIENT_BUILDER.comment("Activate the crafting recipe for the dragon egg").define("dragoneggRecipe", true);
-        ACTIVATE_TOTEM_RECIPES = CLIENT_BUILDER.comment("Activate the crafting recipe for the totem of undying").define("totemRecipe", true);
-        ACTIVATE_DISC_RECIPES = CLIENT_BUILDER.comment("Activate the crafting recipes for music disks").define("diskRecipes", true);
-        ACTIVATE_NETHER_STAR_RECIPES = CLIENT_BUILDER.comment("Activate crafting recipes for the nether star").define("netherStarRecipes", true);
-        ACTIVATE_UNCRAFTING_RECIPES = CLIENT_BUILDER.comment("Activate uncrafting/decrafting recipes for some items and blocks").define("uncraftingRecipes", true);
-
-        CLIENT_BUILDER.pop();
-
-
-        SERVER_CONFIG = SERVER_BUILDER.build();
-        CLIENT_CONFIG = CLIENT_BUILDER.build();
     }
 
 
+    public static void initClient() {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 
-    @SubscribeEvent
-    public static void onLoad(final ModConfigEvent.Loading configEvent) {
+        builder.comment("General").push(CATEGORY_GENERAL);
+        UPDATE_CHECKER = builder.comment("Activate the Update-Checker").define("update-checker", true);
+        builder.pop();
+
+        CLIENT_CONFIG = builder.build();
     }
 
-    @SubscribeEvent
-    public static void onReload(final ModConfigEvent.Reloading configEvent) {
+
+    public static void initServer() {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.comment("General").push(CATEGORY_GENERAL);
+        PATREON_REWARDS = builder.comment("Enables ingame rewards on first spawn for Patreons").define("patreon_rewards", true);
+        builder.pop();
+
+        SERVER_CONFIG = builder.build();
     }
-    **/
+
 }
