af5a2b9bc7bd62a3b83c5d634fff2d8a5d43a460
XxRexRaptorxX <Raptor-Simon@web.de>
2025-08-22 01:32:22 +0200

    feat: optional config datapacks added

--- src/main/java/xxrexraptorxx/main/Uncrafted.java
+++ src/main/java/xxrexraptorxx/main/Uncrafted.java
@@ -1,7 +1,14 @@
 package xxrexraptorxx.main;
 
-import net.neoforged.fml.ModContainer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+import org.jetbrains.annotations.NotNull;
 import xxrexraptorxx.magmacore.main.ModRegistry;
 
 /**
@@ -11,7 +18,82 @@ import xxrexraptorxx.magmacore.main.ModRegistry;
 @Mod(References.MODID)
 public class Uncrafted {
 
-    public Uncrafted(ModContainer container) {
+    public Uncrafted(IEventBus bus) {
         ModRegistry.register(References.MODID, References.NAME, References.URL);
+
+        bus.addListener(Uncrafted::packSetup);
+    }
+
+    public static void packSetup(@NotNull AddPackFindersEvent event) {
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_unbreakable_block_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No unbreakable block recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_admin_items_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No admin item recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_spawn_egg_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No spawn egg recipes!"),
+                PackSource.DEFAULT,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_dragon_egg_recipe"),
+                PackType.SERVER_DATA,
+                Component.literal("No dragon egg recipe!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_nether_star_recipe"),
+                PackType.SERVER_DATA,
+                Component.literal("No nether star recipe!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_debug_stick_recipe"),
+                PackType.SERVER_DATA,
+                Component.literal("No debug stick recipe!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_spawner_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No spawner recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_head_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No head and skull recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_disk_recipes"),
+                PackType.SERVER_DATA,
+                Component.literal("No disk recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(References.MODID, "resourcepacks/no_totem_recipe"),
+                PackType.SERVER_DATA,
+                Component.literal("No totem recipes!"),
+                PackSource.FEATURE,
+                false,
+                Pack.Position.TOP);
     }
 }
